105bf58662c34a74b20e66e74d1c80020d7c96e5
pyruz <pyruz.janbaaz@gmail.com>
2022-08-08 12:42:13 +0400

    Project creation!

---
+++ Domain-Crawler/src/main/java/com/pyruz/multidomains/crawler/DomainCrawlerApplication.java
@@ -0,0 +1,12 @@
+package com.pyruz.multidomains.crawler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DomainCrawlerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DomainCrawlerApplication.class);
+    }
+}
---
+++ Domain-Processor/src/main/java/com/pyruz/multidomains/processor/DomainProcessorApplication.java
@@ -0,0 +1,12 @@
+package com.pyruz.multidomains.processor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DomainProcessorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DomainProcessorApplication.class);
+    }
+}
